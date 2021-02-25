fdbf4844f372ae83a10863518432e8c901a026a1
brrr28 <pavlo.bogachenko@ukr.net>
2021-02-25 11:45:26 +0200

    actually version

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const crypto = require('crypto');
+
+function randomToken(){
+    var current_date = (new Date()).valueOf().toString();
+    var random = Math.random().toString();
+    var token = crypto.createHash('sha1').update(current_date + random).digest('hex');
+    return token;
+}
 
 const users = [];
+const userTokens = {};
 
 app.use(bodyParser.json());
 
@@ -20,8 +29,16 @@ app.post('/user', function(req, res){
         });
         return;
     }
+    
+    var user = users.find(user => user.email == req.body.email)
+    if (user){
+        res.send({
+            message: 'Пользователь с таким email уже существует'
+        });
+        return;
+    }
 
-    const user = {
+    user = {
         email: req.body.email,
         password: req.body.password 
     };
@@ -29,23 +46,38 @@ app.post('/user', function(req, res){
     res.send(user);
 });
 
+
+
 app.post('/login', function(req, res){
     console.log('Запрос на авторизацию', req.body);
-    if (req.body.email !== users.find())
+    var user = users.find(user => user.email == req.body.email)
+    if (user === undefined){
+        res.send({
+            message: 'Такого пользователя нет'
+        })
+        return;
+    } 
+    if (req.body.password !== user.password){
+        res.send({
+            message: 'Пароль неверный'
+        })
+        return;
+    }
 
-// app.post('/form', (req, res) => {
-//     console.log('Пришел запрос пососать!', req.body)
-//     if (!req.body.name){
-//         res.send({
-//             message: 'Напиши своё имя, хуйло! Ща как уебу!'
-//         });
-//         return;
-//     }
-    
-//     res.send({
-//         message: 'Пасаси, ' + req.body.name
-//     });
-// });
+    var token = randomToken();
+    userTokens[token] = user.email
+    res.send({
+        token: token
+    })  
+}) 
+
+app.get('/user/me', function (req, res){
+    console.log('Запрос на получения данных пользователя')
+    userTokens[req.body]
+})
+
+
+
 
 
 
